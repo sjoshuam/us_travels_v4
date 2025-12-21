1658f1ec4270f78ce1a0464c306614ddea53617c
Josh Mendelsohn <s9@S9.local>
2025-12-21 12:50:59 -0500

    Initialize project directory

---
+++ 1_make_visited_table.py
@@ -0,0 +1,14 @@
+'''Compiles data about visited metro areas (MSA) into a single msa table'''
+
+########## IMPORTS AND SETTINGS
+
+########## DEFINE CORE FUNCTIONS
+
+########## DEFINE MAIN EXECUTION FUNCTION
+
+########## TEST EXECUTION
+if __name__ == '__main__':
+    pass
+
+##########==========##########==========##########==========##########==========
+##########==========##########==========##########==========##########==========
